1469e38244a5f987a33e2c76de816acd34e8d16c
zeph254 <ularezephaniah@gmail.com>
2025-10-20 16:38:25 +0300

    for-loop

---
+++ num.c
@@ -0,0 +1,19 @@
+// for loop with numeric decrement
+/*
+  name:         Owuor Zephania Ulare
+  reg:          PA106/G/29218/25
+  description:  This program displays numbers from 100 to 50 in descending order.
+*/
+
+
+#include<stdio.h>
+
+int main(){
+    int i;
+    for ( i = 100; i >= 50; i--)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+    
+}
